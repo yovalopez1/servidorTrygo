26940c99431afd8178822ffc9c6a1f6a3e1f0d22
JOVANY ALBEIRO LOPEZ ZAPATA <jovany_lopez82101@elpoli.edu.co>
2022-07-21 15:40:30 -0500

    Se realizaron validaciones habitaciones y reservas

--- validators/validacionHabitaciones.js
+++ validators/validacionHabitaciones.js
@@ -1,2 +1,19 @@
+import { validationResult } from ('express-validator');
+import { request,response } from ('express');
 
+//Creo una funcion para detectar errores
+function validarPeticionHabitacion(peticion=request,respuesta=response, next){
+
+    let errores=validationResult(peticion);
+
+    if(!errores.isEmpty()){
+
+        return (respuesta.status(400).json(errores))
+
+    }
+    next();
+
+}
+
+module.exports={validarPeticionHabitacion}
 
---
+++ validators/validacionReservas.js
@@ -0,0 +1,17 @@
+import { validationResult } from ('express-validator');
+import { request,response } from ('express');
+
+function validarPeticionReservas(peticion=request,respuesta=response, next){
+
+    let errores=validationResult(peticion);
+
+    if(!errores.isEmpty()){
+
+        return (respuesta.status(400).json(errores))
+
+    }
+    next();
+
+}
+
+module.exports={validarPeticionReservas}
